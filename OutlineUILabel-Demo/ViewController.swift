cccbbb4fbd47a45ac9563dd126269f3d03d87136
Alan Liu <ryutenu.tokyo@gmail.com>
2022-01-06 15:21:01 +0900

    [Mod] Added NSAttributedString

--- OutlineUILabel-Demo/ViewController.swift
+++ OutlineUILabel-Demo/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let attrString = NSAttributedString(
+            string: "Outline UILabel",
+            attributes: [
+                NSAttributedString.Key.strokeColor: UIColor.white,
+                NSAttributedString.Key.strokeWidth: -5.0,
+                NSAttributedString.Key.foregroundColor: UIColor.purple,
+                NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 50)
+            ])
+        
+        titleLabel.attributedText = attrString
     }
-
-
 }
 
